abf58fa3ccb5d825a62d4ab5a7fca4f83119fbe5
Naman Chadha <namanchadha147@gmail.com>
2025-11-05 01:52:55 +0530

    Updated axios + deployment configs

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,   // ✅ NO QUOTES
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   timeout: 30000,
 });
 
--- backend/main.py
+++ backend/main.py
@@ -32,9 +32,9 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "https://nutri-champ.vercel.app/",
-        "*"
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://nutri-champ-ndda8ozt0-namanchadhas-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
